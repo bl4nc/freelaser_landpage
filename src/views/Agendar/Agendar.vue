2792aa3455538c4ec407d8aa330d25ab8a8f0c8e
bl4nc <40602200+bl4nc@users.noreply.github.com>
2022-02-07 22:50:34 -0300

    Feita a funcionalidade da pg de agendamento, e reativação das rotas

--- src/views/Agendar/Agendar.vue
+++ src/views/Agendar/Agendar.vue
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer/Footer.vue";
 import WhatsappIcon from "@/components/WhatsappIcon/WhatsappIcon.vue";
 import CarroselBanner from "@/components/CarroselBanner/CarroselBanner.vue";
 import Accordion from "@/components/Accordion/Accordion.vue";
+import axios from "../../plugins/axios";
 
 export default {
   name: "Agendar",
@@ -20,11 +21,56 @@ export default {
     return {
       imgs_pc: [{ img: "img/pg_agendar/banner_agendar.png" }],
       imgs_mb: [{ img: "img/pg_agendar/banner_agendar_mb.png" }],
+      form: {
+        nome: null,
+        email: null,
+        celular: null,
+        unidade: null,
+        cidade: null,
+        mensagem: null,
+      },
     };
+  },
+  methods: {
+    SendForm() {
+      const router = this.$router;
+      const swal = this.$swal;
+      swal({
+        showConfirmButton: false,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        title: "Enviando",
+        html: `
+        <div class="spinner-border mt-2 mb-2" style="width: 3rem; height: 3rem; overflow:hidden;" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+        `,
+      });
+      axios
+        .post("../teste/api/insertAgendamento", this.form)
+        // .post("http://localhost:8081/insertAgendamento", this.form)
+        .then(function (resp) {
+          swal({
+            icon: "success",
+            showConfirmButton: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            title: resp.data.message,
+          });
+          router.push("/agradecimento");
+        })
+        .catch(function (err) {
+          swal({
+            icon: "error",
+            showConfirmButton: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            title: err,
+          });
+        });
+    },
   },
 };
 </script>
 
-<style scoped>
-@import url("./Agendar.css");
-</style>
+<style scoped src="./Agendar.css"></style>
